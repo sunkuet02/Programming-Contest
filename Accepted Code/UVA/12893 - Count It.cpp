e6fe667dc55188b6c1152dc2f11671c0a9e504b8
Md.Abdulla-Al-Sun <sunkuet02@gmail.com>
2016-07-25 14:38:45 +0600

    another solution

--- Accepted Code/UVA/12893 - Count It.cpp
+++ Accepted Code/UVA/12893 - Count It.cpp
@@ -1,50 +1,149 @@
-   /* ******************************************************
-   *  Md. Abdulla-Al-Sun
-   *  Codeforces: sun.  , Topcoder : sun.
-   *  Uva: sun. , SPOJ : sun_kuet  , LOJ : 5002 CodeChef: sun_kuet
-   *  KUET, Bangladesh
-   ****************************************************** */
+/* ******************************************************
+*  Md. Abdulla-Al-Sun
+*  KUET, Bangladesh
+****************************************************** */
 
 #include <bits/stdc++.h>
-#define scan(a) scanf("%d",&a)
+#define sc(a) scanf("%d",&a)
 #define rep0(i,n) for(int i = 0; i<n ;i++)
-#define rep1(i,n) for(int i = 1; i<=n ;i++)
 #define rep(i,a,b) for(int i = a; i<=b ;i++)
-#define READ() freopen("input.txt","r",stdin);
-int setbit(int N,int pos) { return N = N|(1<<pos); }
-int resetbit(int N, int pos){ return N = N & ~(1<<pos) ;}
-bool checkbit(int N,int pos) { return (bool) (N & 1<<pos) ;}
+#define Read freopen("input.txt","r",stdin);
+#define Write freopen("output.txt","w",stdout);
+#define mem(a,val) memset(a,val, sizeof(a) )
 #define LL long long int
-#define mx 1005
+#define mxx 100005
+#define sz 200005
+#define mod 10056
+#define MAX 1000005
+#define inf INT_MAX
+
 using namespace std;
 
-LL fact[21];
-int f()
+typedef pair<int , int> pii;
+
+
+template<class T> inline T power(T base, int p)
 {
-    fact[0] = 1;
-    for(int i = 1; i<=20 ; i++ )
+    T f = 1 ;
+    for(int i=1 ; i<=p ; i++) f*=base;
+    return f;
+}
+template <typename T>  string NumberToString ( T Number )
+{
+    ostringstream ss;
+    ss << Number;
+    return ss.str();
+}
+
+int setbit(int N,int pos)
+{
+    return N = N|(1<<pos);
+}
+int resetbit(int N, int pos)
+{
+    return N = N & ~(1<<pos) ;
+}
+bool checkbit(int N,int pos)
+{
+    return (bool) (N & (1<<pos ) ) ;
+}
+int noofsetbits(int N)
+{
+    return __builtin_popcount (N);
+}
+/*****************Seive Prime********************/
+/*
+bool prime[MAX];
+void seive()
+{
+    for(int i=2;i<=MAX;i++)
+        prime[i]=true;
+    for(int i=4;i<=MAX;i+=2) prime[i]=false;
+    for(int i=3;i<=sqrt(MAX);i+=2)
     {
-        fact[i] = fact[i-1]*i;
+        if(prime[i]==true)
+        for(int j=i*i;j<MAX;j+=(2*i))
+            prime[j]=false;
     }
+}
+
+*/
+/*********************nCr**************************/
+/*
+long long ncr[1005][1005];
+void NCR()
+{
+    rep0(i,1003)
+    {
+        ncr[i][0] = 1;
+    }
+    rep(i,1, 1003)
+    {
+        rep(j,1,1003)
+        {
+            ncr[i][j] = ncr[i-1][j] + ncr[i-1][j-1];
+            ncr[i][j] %=mod;
+        }
+    }
+}
+*/
+
+LL bigMod(LL N,LL M,LL MOD)
+{
+    if(M==0) return 1;
+
+    if((M/2)*2==M)
+    {
+        LL ret = bigMod(N,M/2,MOD)%MOD;
+        return (ret*ret)%MOD;
+    }
+    else return ((N%MOD)*bigMod(N,M-1,MOD)%MOD)%MOD;
+}
+LL modInverseBigMod(LL a,LL m)  //a*x=1(mod m)
+{
+    return bigMod(a,m-2,m);
+}
+
+/****************************ExtendedEuclid&ModularInverse****/
+
+pii extendedEuclid(LL a,LL b)
+{
+    if(b==0) return pii(1,0);
+    pii d = extendedEuclid(b,a%b);
+    return pii( d.second, d.first - d.second *(a/b) );
+}
+LL modularInverse(LL a,LL m)
+{
+    pii ret = extendedEuclid(a,m);
+    return ( (ret.first % m) + m ) %m ;
+}
+
+/*********************temp_Definition *************************/
+/*
+int dx[] = {-1,0,1,0};
+int dy[] = {0,1,0,-1};
+
+
+  // Knight Moves
+int dx[] = {1,1,2,2,-1,-1,-2,-2};
+int dy[] = {2,-2,1,-1,2,-2,1,-1};
+*/
+/****************************Implementation************************/
+
+LL getCount(LL n)
+{
+    if (n==0) return 0;
+    return getCount(n/2L) + n%2;
 }
 
 int main()
 {
     int test;
-    scan(test);
-    rep1(t,test)
+    sc(test);
+    rep(t, 1, test)
     {
-        LL a;
-        cin>>a;
-        int sum = 0 ;
-        while(a)
-        {
-            sum += (int)(a%2);
-            a/=2;
-        }
-        cout<<sum<<endl;
-
+        LL n ;
+        scanf("%ld",&n);
+        printf("%lld\n", getCount(n));
     }
-
 }
-
