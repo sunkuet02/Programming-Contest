1706e92a67687e50c9840965939f2b369b5fb443
Md.Abdulla-Al-Sun <sunkuet02@gmail.com>
2018-05-02 01:34:48 +0600

    updated with correct code

--- Accepted Code/LightOJ/Computational Geometry/1292 - Laser Shot.cpp
+++ Accepted Code/LightOJ/Computational Geometry/1292 - Laser Shot.cpp
@@ -1,43 +1,63 @@
 #include <bits/stdc++.h>
+#define pii pair<long long, long long>
 
 using namespace std;
 
-int get_order(int a, int b) {
-    if(a - b < 0) return -1;
-    return 1;
-}
+vector<pii> points;
 
-bool is_ordered(int a[]) {
-    int order = get_order(a[1], a[0]); 
+struct coeff {
+    long long a, b, c;
+};
+
+coeff cal_coeff(pii one, pii two) {
+    coeff res;
     
-    for(int i = 2; i < 10; i ++) {
-        if(get_order(a[i], a[i-1]) != order) {
-            return false;
-        }
-    }
+    res.a = one.second - two.second;
+    res.b = two.first - one.first;
     
-    return true;
+    res.c = one.first * (one.second - two.second) - one.second * (one.first - two.first);
+    return res;
+} 
+
+bool inlinee(coeff l1, pii p) {
+    if(p.first * l1.a + p.second * l1.b == l1.c) return true;
+    return false;
 }
 
 int main()
 {
     //freopen("input.txt", "r", stdin);
     
-    int tests, a[10];
+    int test;
+    cin >> test;
     
-    cin >> tests;
-    
-    cout << "Lumberjacks:" << endl;
-    while(tests --) {
-        for(int i = 0; i < 10 ; i++) {
-            cin >> a[i];
+    for(int t = 1; t <= test; t++) {
+        points.clear();
+        int n;
+        cin >> n;
+        
+        for(int i = 0; i < n ; i++) {
+            pii a;
+            cin >> a.first >> a.second;
+            points.push_back(a);
         }
         
-        if(is_ordered(a)) {
-            cout << "Ordered" << endl;
-        } else {
-            cout << "Unordered" << endl;
+        if(n <= 2) {
+            cout << "Case " << t << ": " << n << endl;
+            continue;
         }
-        
+        int mx = 0;
+        for(int i = 0; i < n; i++ ) {
+            for(int j = i + 1; j < n; j++) {
+                coeff ab = cal_coeff(points[i], points[j]);
+                int cnt = 0;
+                for(int k = j + 1; k < n; k++) {
+                    if(inlinee(ab, points[k])) cnt ++;
+                    //if(cnt > 0) cout << i << " " << j << " " << k << endl;
+                }
+                mx = max(mx, cnt);
+            }
+        }
+        cout << "Case " << t << ": " << mx + 2 << endl;
     }
 }
